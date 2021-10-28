cb5eb349cac30527ecb66002c304a9bdef79d2c0
Bruno Santiago <brunosantiago9988@gmail.com>
2021-10-28 19:29:50 -0300

    my first commit

---
+++ Iniciante/1001/1001.js
@@ -0,0 +1,15 @@
+var name = 'bruno';
+console.log(name);
+
+var A = Number(prompt('digite sua idade'));
+var B = Number(prompt('digite a sua altura'));
+
+var resultado = A + B
+console.log(resultado);
+
+
+
+
+
+
+
