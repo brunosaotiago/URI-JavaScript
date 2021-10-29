6501ddeb31478917e6b4a673216ac48c309e603b
Bruno Santiago <brunosantiago9988@gmail.com>
2021-10-28 21:04:28 -0300

    mais exercícios da URI

---
+++ Iniciante/1002/1002.js
@@ -0,0 +1,7 @@
+const pi = 3.14159;
+
+let raio = parseFloat(prompt('digite seu raio'));
+
+let resultado = raio * raio * pi;
+
+console.log('A=' + resultado);
---
+++ Iniciante/1003/1003.js
@@ -0,0 +1,7 @@
+let A = parseInt(prompt('digite um valor inteiro'));
+let B = parseInt(prompt('digite outro valor inteiro'));
+
+let SOMA = A + B;
+console.log('SOMA= ' + SOMA);
+
+
